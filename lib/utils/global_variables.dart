491bb643ec18db4990531fa73e4be92069429d5f
denizedizcan <70265563+denizedizcan@users.noreply.github.com>
2024-01-03 20:20:24 +0300

    global variables

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,16 @@
+const Map<String, dynamic> coinVariablesList = {
+  "bitcoin": "1",
+  "ethereum": "1027",
+  "bnb": "1839",
+  "xrp": "52",
+  "solana": "5426",
+  "cardano": "2010",
+  "avalanche": "5805",
+  "dogecoin": "74",
+  "tron": "1958",
+  "polkadot": "6636",
+};
+
+const String baseUrl =
+    'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest';
+const String apiKey = '';
